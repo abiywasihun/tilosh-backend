2643467ad47942289800f9caa9e3521bc5b33bb5
Abiy <abiywasihun0921@gmail.com>
2024-06-09 22:33:48 +0300

    database config changed

--- app/config/db.js
+++ app/config/db.js
@@ -3,15 +3,15 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 const POSTGRES_URL = process.env.DATABASE_URL;
 //For production development
-// const sequelize = new Sequelize('tilosh', 'tilosh', 'tilsoh@1234', {
-//   host: 'localhost',
-//   dialect: 'postgres'
-// });
-//For local development
-const sequelize = new Sequelize('tilosh', 'postgres', 'postgres', {
+const sequelize = new Sequelize('tilosh', 'tilosh', 'tilsoh@1234', {
   host: 'localhost',
   dialect: 'postgres'
 });
+//For local development
+// const sequelize = new Sequelize('tilosh', 'postgres', 'postgres', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
 // const sequelize = new Sequelize(POSTGRES_URL);
 
 async function connectDB() {
